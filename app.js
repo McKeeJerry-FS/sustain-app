08da2788ed10f4e208adc32c09ba1678bb74a941
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-04-15 22:20:33 -0400

    building out the journals view

--- app.js
+++ app.js
@@ -59,8 +59,8 @@ app.use((req, res, next) => {
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error(err));
