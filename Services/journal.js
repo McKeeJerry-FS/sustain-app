a400c737e238a365dd737e88a88d181cea5430d2
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-04-15 20:20:22 -0400

    setup a function to get a single journal entry by the id

--- Services/journal.js
+++ Services/journal.js
@@ -5,9 +5,21 @@ module.exports = {
   getJournalEntry: async () => {
     const data = await db('journalentries')
       .select('*')
-      .where('user_id', req.user.id)
+      .where({
+        'user_id': req.user.id
+      })
       .orderBy('date', 'desc');
     return data;
   },
   
+  getJournalEntryById: async (id) => {
+    const data = await db('journalentries')
+      .select('*')
+      .where({
+        'id': id, 
+        'user_id': req.user.id,
+      })
+      .first();
+    return data;
+  },
 }
